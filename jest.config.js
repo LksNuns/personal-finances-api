bcca4f74a8235133929d01d6606bb04d6ac1a25c
Lucas <lksnunss@gmail.com>
2021-02-04 07:52:38 -0300

    Initial jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { resolve } = require('path');
+
+// eslint-disable-next-line no-undef
+const root = resolve(__dirname);
+
 // eslint-disable-next-line no-undef
 module.exports = {
-  preset: 'ts-jest',
+  rootDir: root,
+  displayName: 'root-tests',
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
   testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/test/$1',
+  },
 };
