83df8211740da1f8774afb1cfc67a919a454e344
Lucas <lksnunss@gmail.com>
2021-02-08 10:40:10 -0300

    Fixing development Dockerfile, configure database

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,13 @@ FROM node:15.8-alpine3.12
 
 WORKDIR /app
 
+
 COPY package.json package.json
 COPY yarn.lock yarn.lock
 
-RUN yarn install
+# running app in development environment
+ENV NODE_ENV development
 
+RUN yarn install
 
 CMD ["yarn", "run", "dev"]
--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,15 @@ import '../module-alias.config';
 import { Server } from '@overnightjs/core';
 import { Application } from 'express';
 import bodyParser from 'body-parser';
+import * as http from 'http';
+
+import { createConnection } from 'typeorm';
 
 import { TransactionsController } from './controllers/transactions';
 
 export class SetupServer extends Server {
+  private server?: http.Server;
+
   constructor(private port = 3000) {
     super();
   }
@@ -16,18 +21,29 @@ export class SetupServer extends Server {
    * this way we allow the server to be used in tests and normal initialization
    */
   public async init(): Promise<void> {
+    this.setupDatabase();
     this.setupExpress();
     this.setupControllers();
   }
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
-    this.setupControllers();
   }
 
   private setupControllers(): void {
     const transactionController = new TransactionsController();
     this.addControllers([transactionController]);
+  }
+
+  private async setupDatabase(): Promise<void> {
+    await createConnection();
+  }
+
+  public start(): void {
+    this.server = this.app.listen(this.port, () => {
+      console.log(`starting server on ${this.port}`);
+      // logger.info('Server listening on port: ' + this.port);
+    });
   }
 
   public getApp(): Application {
