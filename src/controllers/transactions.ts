14697290e415d56d41617f37251a34b71523b411
Lucas <lksnunss@gmail.com>
2021-02-10 15:44:24 -0300

    Adding transaction#index endpoint

--- src/controllers/transactions.ts
+++ src/controllers/transactions.ts
@@ -1,17 +1,29 @@
 import { CreateTransactionDto } from '@/dto/create-transaction.dto';
 import { TransactionRepository } from '@/repositories/transactions';
-import { Body, Post, JsonController } from 'routing-controllers';
+import { Body, Post, JsonController, Get } from 'routing-controllers';
 import { getCustomRepository } from 'typeorm';
 
 @JsonController('/transactions')
 export class TransactionsController {
+  private readonly transactionRepository: TransactionRepository;
+
+  constructor() {
+    this.transactionRepository = getCustomRepository(TransactionRepository);
+  }
+
+  @Get('')
+  public async index(): Promise<any> {
+    return this.transactionRepository.findAll();
+  }
+
   @Post('')
   public async create(@Body() data: CreateTransactionDto): Promise<any> {
-    const transactionRepository = getCustomRepository(TransactionRepository);
-
     try {
-      await transactionRepository.create(data);
-    } catch (error) {}
+      await this.transactionRepository.create(data);
+    } catch (error) {
+      console.log(error);
+      // TODO Define errors
+    }
 
     return { ok: 'ok' };
   }
--- test/requests/transactions.spec.ts
+++ test/requests/transactions.spec.ts
@@ -1,8 +1,43 @@
-import { Transaction } from '@/entities/transaction';
-import { getConnection } from 'typeorm';
+import { TypeEnum } from '@/entities/transaction';
+import { TransactionRepository } from '@/repositories/transactions';
+import { getCustomRepository } from 'typeorm';
 
 describe('/transactions', () => {
-  describe('when create a new transaction', () => {
+  let transactionRepository: TransactionRepository;
+
+  beforeAll(() => {
+    transactionRepository = getCustomRepository(TransactionRepository);
+  });
+
+  describe('#index', () => {
+    beforeEach(async () => {
+      // TODO Adding factory-girl to improve.
+      await transactionRepository.create({
+        description: 'Shopping',
+        type: TypeEnum.outcome,
+        value: 20.44,
+        executedAt: '2020-09-09',
+      });
+
+      await transactionRepository.create({
+        description: 'Salary',
+        type: TypeEnum.income,
+        value: 1200,
+        executedAt: '2020-09-07',
+      });
+    });
+
+    it('lists transactions', async () => {
+      const response = await global.testRequest.get('/transactions');
+
+      expect(response.body).toMatchObject([
+        { description: 'Shopping', type: 'outcome' },
+        { description: 'Salary', type: 'income' },
+      ]);
+    });
+  });
+
+  describe('#create', () => {
     describe('with valid params', () => {
       it('creates a new transaction', async () => {
         const params = {
@@ -12,17 +47,15 @@ describe('/transactions', () => {
           executedAt: '2020-09-09',
         };
 
-        const repository = getConnection().getRepository(Transaction);
-
         // // Sanity check
-        expect(await repository.count()).toBe(0);
+        expect(await transactionRepository.count()).toBe(0);
 
         const response = await global.testRequest
           .post('/transactions')
           .send(params);
 
         expect(response.status).toBe(200);
-        expect(await repository.count()).toBe(1);
+        expect(await transactionRepository.count()).toBe(1);
 
         // find first
         // expect return serialized finded transaction
